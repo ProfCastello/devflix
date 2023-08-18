172928d764e751efafedb0fbc8f27759f7358a1f
ProfCastello <gustavo.ccastello@outlook.com>
2023-08-18 09:28:41 -0300

    desenvolvido componente movieCard adaptações no App.jsx criado arquivo movieCard.module.css falta código no movieCard.module.css

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
+
 import logo from "../assets/devflix.png";
 import searchIcon from "../assets/search.svg";
 
 import "./App.css";
+import MovieCard from "../components/movieCard/movieCard";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
 
   const apiKey = "e4d577fa";
   const apiUrl = `https://omdbapi.com/?apikey=${apiKey}`;
@@ -19,6 +22,7 @@ const App = () => {
     const data = await response.json();
 
     console.log(data);
+    setMovies(data.Search);
   };
 
   const handleKeyPress = (e) => {
@@ -43,6 +47,17 @@ const App = () => {
           onClick={() => searchMovies(searchTerm)}
         />
       </div>
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard key={movie.imdbID} movies={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>Nenhum filme encontrado 😢</h2>
+        </div>
+      )}
     </div>
   );
 };
